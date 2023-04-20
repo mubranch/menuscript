d791e9de5b38a5ced4726b914101b0f2ed93c91b
Mustafa <mustafa.branch@gmail.com>
2023-04-20 09:48:36 -0600

    small updates

--- src/resources/data/settings.py
+++ src/resources/data/settings.py
@@ -6,9 +6,12 @@ def init():
 
     global data_path
     data_path = os.path.dirname(__file__)
+    print(data_path)
     global image_path
-    image_path = data_path.split("src")[0] + "src/resources/imgs"
+    image_path = data_path.split("data")[0] + "imgs"
+    print(image_path)
     global app_path
-    app_path = data_path.split("data")[0]
+    app_path = data_path.split("resources")[0]
+    print(app_path)
     global first_start
     first_start = False
